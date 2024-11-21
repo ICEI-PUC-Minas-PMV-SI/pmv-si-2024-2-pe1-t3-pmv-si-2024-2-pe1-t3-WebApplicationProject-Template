13376edac3c511c5d1c41a83ae57956006b70ff9
Guilherme Lacerda <guilherme.lacerda@sga.pucminas.br>
2024-11-21 17:07:32 -0300

    Add validação teste para exibir ou não exibir determinado conteúdo na página

--- src/script.js
+++ src/script.js
@@ -1,3 +1,16 @@
 function showNews() {
-alert('funcionando')
+
+    window.localStorage.setItem('nome', 'Fulano da silva')    
+    
+    const hotNews = document.getElementsByClassName('hotNews')[0]
+    
+    
+    if (localStorage) {
+        hotNews.style.display = 'none'
+        alert('tem dado no local storage o nome do usuário é ' + window.localStorage.getItem('nome'))
+
+    } else {
+        alert('nao tem dado no localstorage')
+    }
+
 }
