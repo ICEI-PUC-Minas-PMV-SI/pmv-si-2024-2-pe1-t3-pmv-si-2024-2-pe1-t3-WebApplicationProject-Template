17592668277fede0af02e30e92167ce80b7e8a0e
Guilherme Lacerda <guilherme.lacerda@sga.pucminas.br>
2024-11-22 13:19:37 -0300

    Add Lógica para botão logado e deslogado

--- src/script.js
+++ src/script.js
@@ -1,11 +1,31 @@
 function showNews() {
 
-    const hasLoggedUser = localStorage.getItem('nome') ? true : false
+    const hasLoggedUser = localStorage.getItem('isLogged') 
     const hotNews = document.getElementsByClassName('hotNews')[0]
     
-    console.log(hasLoggedUser)
+    console.log(hasLoggedUser == 'true' ? 'logado' : 'deslogado')
 
-    if (hasLoggedUser) {
+    if (hasLoggedUser == 'true' ) {
         hotNews.style.display = 'none'
     } 
+}
+
+function hasLogin() {
+    const logIn = document.getElementsByClassName('logInButton')[0]
+    const logOut = document.getElementsByClassName('logOutButton')[0]
+
+    window.localStorage.setItem('nome', 'joão')
+    window.localStorage.setItem('isLogged', 'false')
+
+    const isLogged = localStorage.getItem('isLogged')
+
+
+
+
+    if (isLogged == 'true') {
+        logIn.style.display = 'flex'
+    } else {
+        logOut.style.display = 'flex'
+    }
+   
 }
